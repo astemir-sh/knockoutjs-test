8d10eed166f9c43de0ae3db9cec64e1d981cbe16
astemir-sh <astemirshereuzhev@gmail.com>
2015-08-28 09:41:22 +0300

    +

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 function AppViewModel(){
 	this.firstName = ko.observable("Astemir");
 	this.lastName = ko.observable("Shereuzhev");
+	this.sex = ko.observable("");
+	this.isAdmin = ko.observable(true);
+
+
+	this.sexOptions = ['Мужской', 'Женский'];
+
+	this.fullName = ko.computed(function(){
+		return this.lastName() + ' ' + this.firstName();
+	},this)
 };
 
 ko.applyBindings(new AppViewModel());
